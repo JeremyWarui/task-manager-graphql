f3455192b17f1f589b746c7ab227c99a81639dae
JeremyWarui <jerywarne7@gmail.com>
2025-07-24 18:28:48 +0300

    Implement creation of new task and user. Implement the login function in the resolver to return a token

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 const User = require('./models/user')
 const Task = require('./models/task')
+const { GraphQLError } = require('graphql')
+const jwt = require('jsonwebtoken')
 
 require('dotenv').config()
 
@@ -41,6 +43,7 @@ const typeDefs = `
     type Query {
         dummy: String
         taskCount: Int!
+        allUsers: [User!]!
         allTasks(status: String): [Task!]!
         me: User
     }
@@ -57,7 +60,12 @@ const typeDefs = `
 
         addTask(
             title: String!
-            description: String
+            description: String!
+            done: Boolean!
+        ): Task
+
+        editTask(
+            id: ID!
             done: Boolean!
         ): Task
     }
@@ -67,14 +75,97 @@ const resolvers = {
   Query: {
     dummy: () => 'Hello World',
     taskCount: async () => await Task.countDocuments(),
+    allUsers: async () => await User.find({}),
     allTasks: async (root, args) => {
-      return await Task.find({}).populate('user')
+      let filter = {}
+      if (args.status === 'done') {
+        filter.done = true
+      }
+      if (args.status === 'pending') {
+        filter.done = false
+      }
+      return await Task.find(filter).populate('user')
     },
     me: (root, args) => 'current user',
   },
   User: {
     tasks: async (user) => {
       return await Task.find({ user: user.id })
+    },
+  },
+  Mutation: {
+    addTask: async (root, args) => {
+      try {
+        const task = new Task({
+          title: args.title,
+          description: args.description,
+          done: args.done || false,
+        })
+
+        await task.save()
+
+        return task
+      } catch (error) {
+        let message = 'Saving new task failed'
+        if (error.name === 'ValidationError') {
+          message =
+            'title(minimum of 5 characters) and done is required; Description of atleast 5 characters'
+        }
+        throw new GraphQLError(message, {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          },
+        })
+      }
+    },
+    editTask: async (root, args) => {
+      try {
+        const task = await Task.findByIdAndUpdate(
+          { _id: args.id },
+          { done: args.done },
+          { new: true }
+        )
+        return task
+      } catch (error) {
+        throw new GraphQLError('task not found', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            error,
+          },
+        })
+      }
+    },
+
+    createUser: async (root, args) => {
+      const user = new User({ username: args.username })
+      try {
+        return await user.save()
+      } catch (error) {
+        let message = 'Creating user failed'
+        if (error.code === 11000) message = 'username must be unique'
+        throw new GraphQLError(message, {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.username,
+            error,
+          },
+        })
+      }
+    },
+    login: async (root, args) => {
+      const user = await User.find({ username: args.username })
+
+      if (!user && args.password !== 'secret') {
+        throw new GraphQLError('wrong credentials', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          },
+        })
+      }
+
+      const userPayload = { username: user.username, id: user._id }
+      const token = jwt.sign(userPayload, process.env.JWT_SECRET)
+      return { value: token }
     },
   },
 }
